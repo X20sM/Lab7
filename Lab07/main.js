ac20092184fee60b2e7e97131f62e2e7f7da3dd6
RashedAsh <134154071+X20sM@users.noreply.github.com>
2023-11-04 22:13:45 +0300

    Update main.js

--- Lab07/main.js
+++ Lab07/main.js
@@ -1,7 +1,7 @@
-const text = document.querySelector("h1");
-const button = document.querySelector("button");
-const form = document.querySelector("form");
-const input = document.querySelector("input");
+const text = document.querySelector(".h1");
+const button = document.querySelector(".button");
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
 if (form) {
   form.addEventListener("submit", function (e) {
     e.preventDefault();
